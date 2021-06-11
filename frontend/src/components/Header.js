37c005d64d6643d4aecde7fcbf92e721dbd8f1ca
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-06-10 17:52:55 -0700

    added backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
+
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-  <Navbar.Brand href="/">Stone Tool Direct</Navbar.Brand>
+
+    <LinkContainer to="/">
+  <Navbar.Brand>Stone Tool Direct</Navbar.Brand>
+  </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/home">Home</Nav.Link>
-      <Nav.Link href="/products">Products</Nav.Link>
-      <Nav.Link href="/aboutus">About Us</Nav.Link>
-      <Nav.Link href="/contact">Contact Us</Nav.Link>
-      <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>  Cart</Nav.Link>
-      <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+        <LinkContainer to="/">
+      <Nav.Link>Home</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/products">
+      <Nav.Link>Products</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/aboutus">
+          <Nav.Link>About Us</Nav.Link>
+      </LinkContainer>
+
+      <LinkContainer to="/contactus">
+          <Nav.Link>Contact Us</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/cart">
+      <Nav.Link><i className="fas fa-shopping-cart"></i>  Cart</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/login">
+      <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+      </LinkContainer>
     </Nav>
     
   </Navbar.Collapse>
