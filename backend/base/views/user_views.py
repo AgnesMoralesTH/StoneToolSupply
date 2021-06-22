2916dd5655558228926431676823103ff722b862
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-06-22 04:15:23 -0700

    added url and views folders for users and products, added reducers and actions for users and added a userInfo object to localstorage

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,20 +1,21 @@
 from django.contrib.auth.models import User, update_last_login
 from django.shortcuts import render
-from django.http import JsonResponse
+
 from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .models import Product
-from .serializers import ProductSerializer,UserSerializer, UserSerializerWithToken
-from .products import products
+
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.products import products
 from django.contrib.auth.hashers import make_password
 from  rest_framework import status
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-# Create your views here.
+
+
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
@@ -46,21 +47,7 @@ def getUsers(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
 
-
-
-    
-@api_view(['GET'])
-def getProduct(request, pk):
-    products = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(products, many=False)
-   
-    return Response(serializer.data)
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -76,4 +63,3 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
